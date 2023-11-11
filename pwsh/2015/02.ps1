7d3ad89eeafc130760d9be6fa7ff7a9f9c1dee9e
Andy Fallon <andy.m.fallon@gmail.com>
2023-11-11 15:32:25 +0000

    part 2 completed

--- pwsh/2015/02.ps1
+++ pwsh/2015/02.ps1
@@ -2,6 +2,7 @@
 $inputdata = Get-Content .\inputs\2015\02.txt
 
 $totalpaperrequired = 0
+$totalrequiredribbon = 0
 
 foreach ($entry in $inputdata) {
     
@@ -27,6 +28,22 @@ foreach ($entry in $inputdata) {
 
 
     $totalpaperrequired += $requiredpaper
+
+    # Part 2
+    # The ribbon required to wrap a present is the shortest distance around its sides,
+    # or the smallest perimeter of any one face. 
+    # Each present also requires a bow made out of ribbon as well;
+    # the feet of ribbon required for the perfect bow is equal to the cubic feet of volume of the present.
+
+    # A present with dimensions 2x3x4 requires 2+2+3+3 = 10 feet of ribbon to wrap the present plus 2*3*4 = 24 feet of ribbon for the bow, for a total of 34 feet.
+    # sort sides by shorest side
+
+    $sortedsides = $length, $width, $height | Sort-Object 
+    $ribbon = $sortedsides[0] + $sortedsides[0] + $sortedsides[1] + $sortedsides[1]
+    $bow = $sortedsides[0] * $sortedsides[1] * $sortedsides[2]
+    $requiredribbon = $ribbon + $bow
+    $totalrequiredribbon += $requiredribbon
 }
 
 $totalpaperrequired
+$totalrequiredribbon
